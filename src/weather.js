2e922fbac89f2966a853bf679dde364906ea8e71
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-08-13 05:52:59 +0900

    weather js

--- src/weather.js
+++ src/weather.js
@@ -4,10 +4,21 @@ function onGeoOk(position){
   const lon=position.coords.longitude;
   const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url).then((response)=>response.json()).then((data)=>{
-    const weather=document.querySelector(".weather span:first-child");
-    const city=document.querySelector(".weather span:last-child");
+    const city=document.getElementById("weatherCity");
+    const weather=document.getElementById("weatherTitle");
+    const icon=document.getElementById("weahterIcon");
+    const temp=document.getElementById("weahterTemp");
     city.innerText=data.name;
-    weather.innerText=`${data.weather[0].main}/${data.main.temp}`;});
+    weather.innerText=`${data.weather[0].main}`;
+    paintWeatherIcon(icon,`${data.weather[0].icon}`);
+    temp.innerText=`${data.main.temp} \u00B0`;
+  });
+}
+
+function paintWeatherIcon(icon,iconNum){
+  iconNum=iconNum.substring(0,2);
+  console.log(iconNum);
+  icon.src=`http://openweathermap.org/img/wn/${iconNum}d@2x.png`;
 }
 function onGeoError(){
   alert("Could not load location.")
