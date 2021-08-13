a0976844e98f273a37106e93af6453845adf6d3b
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-08-13 10:05:30 +0900

    edit name function

--- src/name.js
+++ src/name.js
@@ -1,11 +1,12 @@
 const nameForm=document.querySelector(".name__form");
 const nameInput=document.querySelector(".name__input");
-const greetText=document.querySelector(".name__text");
+const greetText=document.querySelector(".greeting");
+const editNameBtn=document.getElementById("nameEdit");
 const NAME_KEY="name";
 function greeting(name){
   nameForm.classList.add("hidden");
   greetText.classList.remove("hidden");
-  greetText.innerText=`Hello, ${name} `;
+  greetText.querySelector(".name__text").innerText=`Hello, ${name} `;
 }
 function handleNameSubmit(event){
   event.preventDefault();
@@ -14,10 +15,19 @@ function handleNameSubmit(event){
   localStorage.setItem(NAME_KEY,name);
 
 }
+function editName(){
+  nameForm.classList.remove("hidden");
+  greetText.classList.add("hidden");
+  nameInput.value="";
+  nameForm.addEventListener("submit",handleNameSubmit);
+}
 
-nameForm.addEventListener("submit",handleNameSubmit);
+
 const savedName=localStorage.getItem(NAME_KEY);
 if(savedName!==null){
   
   greeting(savedName);
 }
+
+nameForm.addEventListener("submit",handleNameSubmit);
+editNameBtn.addEventListener("click",editName);
